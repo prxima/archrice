4e515ccb53eda862bbcc2123229d4619426e5188
prxima <72438791+prxima@users.noreply.github.com>
2023-03-08 08:17:39 +0100

    Add files via upload

---
+++ zsh-syntax-highlighting/nightgfly-zsh-syntax-highlighting.zsh
@@ -0,0 +1,70 @@
+# Paste this files contents inside your ~/.zshrc before you activate zsh-syntax-highlighting
+ZSH_HIGHLIGHT_HIGHLIGHTERS=(main cursor)
+typeset -gA ZSH_HIGHLIGHT_STYLES
+
+## General
+### Diffs
+### Markup
+## Classes
+## Comments
+ZSH_HIGHLIGHT_STYLES[comment]='fg=#7c8f8f'
+## Constants
+## Entitites
+## Functions/methods
+ZSH_HIGHLIGHT_STYLES[alias]='fg=#a1cd5e'
+ZSH_HIGHLIGHT_STYLES[suffix-alias]='fg=#a1cd5e'
+ZSH_HIGHLIGHT_STYLES[global-alias]='fg=#a1cd5e'
+ZSH_HIGHLIGHT_STYLES[function]='fg=#a1cd5e'
+ZSH_HIGHLIGHT_STYLES[command]='fg=#a1cd5e'
+ZSH_HIGHLIGHT_STYLES[precommand]='fg=#a1cd5e,italic'
+ZSH_HIGHLIGHT_STYLES[autodirectory]='fg=#ecc48d,italic'
+ZSH_HIGHLIGHT_STYLES[single-hyphen-option]='fg=#ecc48d'
+ZSH_HIGHLIGHT_STYLES[double-hyphen-option]='fg=#ecc48d'
+ZSH_HIGHLIGHT_STYLES[back-quoted-argument]='fg=#cba6f7'
+## Keywords
+## Built ins
+ZSH_HIGHLIGHT_STYLES[builtin]='fg=#a1cd5e'
+ZSH_HIGHLIGHT_STYLES[reserved-word]='fg=#a1cd5e'
+ZSH_HIGHLIGHT_STYLES[hashed-command]='fg=#a1cd5e'
+## Punctuation
+ZSH_HIGHLIGHT_STYLES[commandseparator]='fg=#ff5874'
+ZSH_HIGHLIGHT_STYLES[command-substitution-delimiter]='fg=#acb4c2'
+ZSH_HIGHLIGHT_STYLES[command-substitution-delimiter-unquoted]='fg=#acb4c2'
+ZSH_HIGHLIGHT_STYLES[process-substitution-delimiter]='fg=#acb4c2'
+ZSH_HIGHLIGHT_STYLES[back-quoted-argument-delimiter]='fg=#ff5874'
+ZSH_HIGHLIGHT_STYLES[back-double-quoted-argument]='fg=#ff5874'
+ZSH_HIGHLIGHT_STYLES[back-dollar-quoted-argument]='fg=#ff5874'
+## Serializable / Configuration Languages
+## Storage
+## Strings
+ZSH_HIGHLIGHT_STYLES[command-substitution-quoted]='fg=#f9e2af'
+ZSH_HIGHLIGHT_STYLES[command-substitution-delimiter-quoted]='fg=#f9e2af'
+ZSH_HIGHLIGHT_STYLES[single-quoted-argument]='fg=#f9e2af'
+ZSH_HIGHLIGHT_STYLES[single-quoted-argument-unclosed]='fg=#fc514e'
+ZSH_HIGHLIGHT_STYLES[double-quoted-argument]='fg=#f9e2af'
+ZSH_HIGHLIGHT_STYLES[double-quoted-argument-unclosed]='fg=#fc514e'
+ZSH_HIGHLIGHT_STYLES[rc-quote]='fg=#f9e2af'
+## Variables
+ZSH_HIGHLIGHT_STYLES[dollar-quoted-argument]='fg=#acb4c2'
+ZSH_HIGHLIGHT_STYLES[dollar-quoted-argument-unclosed]='fg=#fc514e'
+ZSH_HIGHLIGHT_STYLES[dollar-double-quoted-argument]='fg=#acb4c2'
+ZSH_HIGHLIGHT_STYLES[assign]='fg=#acb4c2'
+ZSH_HIGHLIGHT_STYLES[named-fd]='fg=#acb4c2'
+ZSH_HIGHLIGHT_STYLES[numeric-fd]='fg=#acb4c2'
+## No category relevant in spec
+ZSH_HIGHLIGHT_STYLES[unknown-token]='fg=#fc514e'
+ZSH_HIGHLIGHT_STYLES[path]='fg=#acb4c2,underline'
+ZSH_HIGHLIGHT_STYLES[path_pathseparator]='fg=#ff5874,underline'
+ZSH_HIGHLIGHT_STYLES[path_prefix]='fg=#acb4c2,underline'
+ZSH_HIGHLIGHT_STYLES[path_prefix_pathseparator]='fg=#ff5874,underline'
+ZSH_HIGHLIGHT_STYLES[globbing]='fg=#acb4c2'
+ZSH_HIGHLIGHT_STYLES[history-expansion]='fg=#cba6f7'
+#ZSH_HIGHLIGHT_STYLES[command-substitution]='fg=?'
+#ZSH_HIGHLIGHT_STYLES[command-substitution-unquoted]='fg=?'
+#ZSH_HIGHLIGHT_STYLES[process-substitution]='fg=?'
+#ZSH_HIGHLIGHT_STYLES[arithmetic-expansion]='fg=?'
+ZSH_HIGHLIGHT_STYLES[back-quoted-argument-unclosed]='fg=#fc514e'
+ZSH_HIGHLIGHT_STYLES[redirection]='fg=#acb4c2'
+ZSH_HIGHLIGHT_STYLES[arg0]='fg=#acb4c2'
+ZSH_HIGHLIGHT_STYLES[default]='fg=#acb4c2'
+ZSH_HIGHLIGHT_STYLES[cursor]='fg=#acb4c2'
